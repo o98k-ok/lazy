cdb6b710286f9f3ea3759c7e257206d5a09346ef
o98k-ok <hggend@gmail.com>
2022-03-25 00:42:23 +0800

    feat: Adding alfred/FlowVariables
    Signed-off-by: o98k-ok <hggend@gmail.com>

---
+++ v2/alfred/config.go
@@ -0,0 +1,44 @@
+package alfred
+
+import (
+	"errors"
+	"github.com/o98k-ok/lazy/v2/mac"
+	"io"
+	"os"
+)
+
+const InfoFile = "info.plist"
+
+func flowVariables(reader io.ReadSeeker) (map[string]string, error) {
+	var res map[string]string
+	variables, err := mac.DefaultsRead(reader, []string{"variables"})
+	if err != nil {
+		return res, err
+	}
+
+	val, ok := variables.(map[string]interface{})
+	if !ok {
+		return res, errors.New("variables format error")
+	}
+
+	res = make(map[string]string, len(val))
+	for k, v := range val {
+		if vv, ok := v.(string); !ok {
+			return res, errors.New("variables type error")
+		} else {
+			res[k] = vv
+		}
+	}
+	return res, nil
+}
+
+func FlowVariables() (map[string]string, error) {
+	var res map[string]string
+	fi1e, err := os.Open(InfoFile)
+	if err != nil {
+		return res, err
+	}
+
+	defer fi1e.Close()
+	return flowVariables(fi1e)
+}
---
+++ v2/alfred/config_test.go
@@ -0,0 +1,70 @@
+package alfred
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestFlowVariables(t *testing.T) {
+	t.Run("test get flow envs", func(t *testing.T) {
+		content := `<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>variables</key>
+	<dict>
+		<key>username</key>
+		<string>o98k-ok</string>
+		<key>ttl</key>
+		<string>8h</string>
+	</dict>
+</dict>
+</plist>`
+		reader := strings.NewReader(content)
+		envs, err := flowVariables(reader)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 2, len(envs))
+		assert.Equal(t, "o98k-ok", envs["username"])
+		assert.Equal(t, "8h", envs["ttl"])
+	})
+
+	t.Run("test format error", func(t *testing.T) {
+		content := `
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>variable</key>
+	<dict>
+		<key>username</key>
+		<string>o98k-ok</string>
+		<key>ttl</key>
+		<string>8h</string>
+	</dict>
+</dict>
+</plist>`
+		_, err := flowVariables(strings.NewReader(content))
+		assert.Error(t, err)
+	})
+
+	t.Run("test format error", func(t *testing.T) {
+		content := `
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>variable
+	<dict>
+		<key>username</key>
+		<string>o98k-ok</string>
+		<key>ttl</key>
+		<string>8h</string>
+	</dict>
+</dict>
+</plist>`
+		_, err := flowVariables(strings.NewReader(content))
+		assert.Error(t, err)
+	})
+}
