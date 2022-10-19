1fa445bc8ece7319f536b10c1fc316985b11fc50
o98k-ok <hggend@gmail.com>
2022-10-19 18:37:50 +0800

    [feat]: add search map function
    Signed-off-by: o98k-ok <hggend@gmail.com>

--- v2/collection/search.go
+++ v2/collection/search.go
@@ -5,13 +5,25 @@ import (
 	"strings"
 )
 
-func Search[T any](items []T, key string) []T {
+func SearchSlice[T any](items []T, key string) []T {
 	var res []T
 	for _, item := range items {
 		dest := strings.ToLower(fmt.Sprint(item))
 		match := strings.ToLower(key)
 		if strings.Contains(dest, match) {
 			res = append(res, item)
+		}
+	}
+	return res
+}
+
+func SearchMap[T any](items map[string]T, key string) map[string]T {
+	res := make(map[string]T)
+	for key, item := range items {
+		dest := strings.ToLower(fmt.Sprint(item))
+		match := strings.ToLower(key)
+		if strings.Contains(dest, match) {
+			res[key] = item
 		}
 	}
 	return res
--- v2/collection/search_test.go
+++ v2/collection/search_test.go
@@ -12,13 +12,13 @@ type TestSearchStruct struct {
 }
 
 func TestSearch(t *testing.T) {
-	assert.Equal(t, []string{"1", "11"}, Search([]string{"1", "2", "3", "11"}, "1"))
+	assert.Equal(t, []string{"1", "11"}, SearchSlice([]string{"1", "2", "3", "11"}, "1"))
 	testcases := []TestSearchStruct{
 		{"shadow", 10},
 		{"tencent", 20},
 		{"work", 100},
 	}
 
-	assert.Equal(t, []TestSearchStruct{{"shadow", 10}, {"work", 100}}, Search(testcases, "10"))
-	assert.Equal(t, []TestSearchStruct{{"shadow", 10}, {"work", 100}}, Search(testcases, "o"))
+	assert.Equal(t, []TestSearchStruct{{"shadow", 10}, {"work", 100}}, SearchSlice(testcases, "10"))
+	assert.Equal(t, []TestSearchStruct{{"shadow", 10}, {"work", 100}}, SearchSlice(testcases, "o"))
 }
