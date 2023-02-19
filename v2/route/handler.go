34bab3cb6fa27504640c252497f940714cfa8f91
o98k <63107263+o98k-ok@users.noreply.github.com>
2023-02-19 14:34:36 +0800

    [feature]<route>: finish simple route pkg (#2)
    Signed-off-by: o98k-ok <hggend@gmail.com>

---
+++ v2/route/handler.go
@@ -0,0 +1,28 @@
+package route
+
+import "github.com/kataras/iris/v12"
+
+// RequestParser parse request from iris
+type RequestParser[T any] interface {
+	Parse(ctx iris.Context, attrs map[string]interface{}) (T, error)
+}
+
+// ContextParser parse context from iris
+type ContextParser[Context any] interface {
+	Parse(ctx iris.Context) (Context, error)
+}
+
+// Handler handle http request
+type Handler[T, Context, R any] func(ctx Context, req T) (R, error)
+
+// ResponseHandler response http
+type ResponseHandler interface {
+	OK(ctx iris.Context, data interface{})
+	ParamErr(ctx iris.Context, err error)
+	Failed(ctx iris.Context, errs error)
+}
+
+// DocHandler doc api document
+type DocHandler[T, R any] interface {
+	DocIt(method string, path string) (string, error)
+}
